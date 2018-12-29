be1bd2c4319e17413b734e3e7b5a46f79dbc2299
HanulGye <kyehl@naver.com>
2018-12-29 12:43:30 +0900

    Initial commit

---
+++ app/src/main/java/tjeit/kr/a20181229_02_activitylifecycle/MainActivity.java
@@ -0,0 +1,55 @@
+package tjeit.kr.a20181229_02_activitylifecycle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends BaseActivity {
+
+    private android.widget.Button moveBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+
+        Toast.makeText(mContext, "Main - onCreate", Toast.LENGTH_SHORT).show();
+        Log.d("Main", "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(mContext, "Main - onResume", Toast.LENGTH_SHORT).show();
+        Log.d("Main", "onResume");
+    }
+
+    @Override
+    public void setupEvents() {
+        moveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, NextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+        this.moveBtn = (Button) findViewById(R.id.moveBtn);
+    }
+}
---
+++ app/src/main/java/tjeit/kr/a20181229_02_activitylifecycle/NextActivity.java
@@ -0,0 +1,56 @@
+package tjeit.kr.a20181229_02_activitylifecycle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class NextActivity extends BaseActivity {
+
+    private android.widget.Button closeBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        bindViews();
+        setupEvents();
+        setValues();
+
+        Toast.makeText(mContext, "Next - onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(mContext, "Next - onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(mContext, "Next - onStop", Toast.LENGTH_SHORT).show();
+    }
+    
+
+    @Override
+    public void setupEvents() {
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+        this.closeBtn = (Button) findViewById(R.id.closeBtn);
+    }
+}
